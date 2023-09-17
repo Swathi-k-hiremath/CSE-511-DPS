596b7e0db4e3ce3aefec8ad89144e61b590d3330
Swathi-k-hiremath <52506061+Swathi-k-hiremath@users.noreply.github.com>
2023-09-17 16:55:34 -0700

    Add files via upload

--- Assignment_2/solution.sql
+++ Assignment_2/solution.sql
@@ -1,9 +1,14 @@
+
+/* Q1: Write a query to return the total number of movies for each genre */
+
 CREATE TABLE query1 AS 
 SELECT g.name, count(1) as moviecount 
 FROM hasagenre 
 JOIN genres g 
 USING (genreid) GROUP BY name;
 
+
+/* Q2: Write a query to return the average rating per genre */
 
 CREATE TABLE query2 AS
 SELECT g.name AS name, avg(rating) AS rating
@@ -13,12 +18,16 @@ JOIN ratings r USING (movieid)
 JOIN genres g USING (genreid)
 GROUP BY g.name;
 
+/* Q3: Write a query to return the movies have at least 10 ratings */
+
 CREATE TABLE query3 AS
 SELECT m.name as title, count(1) countofratings
 FROM ratings
 JOIN movies m USING (movieid)
 GROUP BY m.name
 HAVING count(1) > 9;
+
+/* Q4: Write a query to return all “Comedy” movies, including movieid and title. */
 
 CREATE TABLE query4 AS
 SELECT m.movieid AS movieid, m.name AS title
@@ -27,11 +36,15 @@ JOIN hasagenre h USING (movieid)
 JOIN genres g USING (genreid)
 WHERE g.name = 'Comedy';
 
+/* Q5: Write a query to return the average rating per movie */
+
 CREATE TABLE query5 AS
 SELECT m.name as title, avg(rating) as average
 FROM ratings r
 JOIN movies m USING (movieid)
 GROUP BY m.name;
+
+/* Q6: Write a query to return the average rating for all “Comedy” movies. */
 
 CREATE TABLE query6 AS
 SELECT avg(r.rating) AS average
@@ -40,6 +53,8 @@ JOIN ratings r USING (movieid)
 JOIN hasagenre h USING (movieid)
 JOIN genres g USING (genreid)
 WHERE g.name = 'Comedy';
+
+/* Q7: Write a query to return the average rating for all movies having genre both Comedy and Romance*/
 
 CREATE TABLE query7 AS
 SELECT avg(rating) as average
@@ -52,6 +67,9 @@ JOIN (SELECT movieid
 	GROUP BY m.movieid
 	HAVING count(1) = 2) Y
 USING (movieid);
+
+
+/* Q8: Write a query to return the average rating for all movies having genre "Romance" but not "Comedy" */
 
 CREATE TABLE query8 AS
 SELECT AVG(rating) AS average
@@ -72,6 +90,8 @@ JOIN (SELECT movieid
      ) Y
 USING (movieid);
 
+
+/* Q9: Write a query to return all movies that are rated by a User such that the userId is equal to v1. The v1 will be an integer parameter passed to the SQL query */
 
 CREATE TABLE query9 AS
 SELECT movieid, rating
